8994e54ef0ef34b5efb293698bfddc425e46408b
mandy <hitangman@qq.com>
2017-01-16 20:08:17 +0800

    get joblist from localStorage and list them in the list page

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,7 +1,14 @@
 // pages/list/index.js
+import { getJobs } from '../../utils/jobs';
+
 Page({
-  data:{},
+  data:{
+    jobs: []
+  },
   onLoad:function(options){
+    getJobs().then((jobs) => {
+      this.setData({jobs})
+    });
     // 页面初始化 options为页面跳转所带来的参数
   },
   onReady:function(){
