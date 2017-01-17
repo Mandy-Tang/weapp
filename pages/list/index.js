3854c735ee28ac94514a7971ec8e8ea88e656676
mandy <hitangman@qq.com>
2017-01-17 16:25:21 +0800

    1. remove position page from the tabBar and add the navigation button in list page 2. add edit and delete button in job list

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,26 +1,19 @@
 // pages/list/index.js
 import { getJobs } from '../../utils/jobs';
-
+const goToPositionPage = () => {
+  // remain the current page and navigate to another page which is not in the tabBar
+  wx.navigateTo({
+    url: '../position/index' // relative path
+  })
+}
 Page({
   data:{
-    jobs: []
+    jobs: [],
   },
   onLoad:function(options){
     getJobs().then((jobs) => {
       this.setData({jobs})
     });
-    // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
-  }
+  goToPositionPage
 })
